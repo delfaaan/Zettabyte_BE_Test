c740e8f3c0bfe835f2e5d19cad2ea410256313cb
Delfan Azhar Andhika <16264678+delfaaan@users.noreply.github.com>
2022-01-17 10:47:46 +0700

    Add files via upload

---
+++ code_1.js
@@ -0,0 +1,27 @@
+
+/**
+ * Direction:
+ * Swap these numbers without using temporary variable.
+ * 
+ * Expected Result:
+ * {
+ *  a: 4,
+ *  b: 2
+ * }
+ */
+
+let numbers = {
+    a: 2,
+    b: 4
+};
+
+// this function using xor.
+function result(numbers){
+    numbers.a = numbers.a ^ numbers.b;
+    numbers.b = numbers.a ^ numbers.b;
+    numbers.a = numbers.a ^ numbers.b;
+
+    return numbers;
+}
+
+console.log(result(numbers));
